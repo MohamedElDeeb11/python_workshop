2df19374a9decc7edbcf3864d24e566976fd2f07
Mohamed Eldeeb <167246548+MohamedElDeeb11@users.noreply.github.com>
2026-01-11 12:54:39 +0000

    save work

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,18 +1,13 @@
 import sys
 import pandas as pd
 
-print("args : ", sys.argv)
+print('args : ', sys.argv)
 
-day =int(sys.argv[1])
+day = int(sys.argv[1])
 
+df =pd.DataFrame({'m1':[1,2],'m2':[1,3]})
+df.to_parquet(f"/app/output/output_day_{sys.argv[1]}.parquet")
 
-df = pd.DataFrame({"users": [1, 2], "employ": [3, 4]})
-df['day'] = day
-print(df.head())
-
-
-
-df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
 
 
 print(f'pipeline is running for day {day}')
